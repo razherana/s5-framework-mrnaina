503c96951c2acd86ac38e75c933a9d505fc5e3b8
razherana <razherana@gmail.com>
2025-12-03 02:13:14 +0300

    feat: Added support for record

--- src/main/java/mg/razherana/framework/web/utils/objectconversion/ConversionObjectUtils.java
+++ src/main/java/mg/razherana/framework/web/utils/objectconversion/ConversionObjectUtils.java
@@ -10,6 +10,13 @@ public class ConversionObjectUtils {
   }
 
   public static <T> T convertMapToObject(Map<String, Object> parametersMap, Class<T> targetType, Object outerInstance) {
+    if (targetType == null) {
+      throw new ConversionObjectException("Target type cannot be null");
+    }
+
+    if (targetType.isRecord()) {
+      return convertMapToRecord(parametersMap, targetType, outerInstance);
+    }
 
     Object _instance = null;
     if (targetType.getEnclosingClass() != null && !targetType.isAnonymousClass())
@@ -44,6 +51,47 @@ public class ConversionObjectUtils {
     });
 
     return instance;
+  }
+
+  private static <T> T convertMapToRecord(Map<String, Object> parametersMap, Class<T> targetType,
+      Object outerInstance) {
+    if (!targetType.isRecord()) {
+      throw new ConversionObjectException("Target type is not a record: " + targetType.getName());
+    }
+
+    var recordComponents = targetType.getRecordComponents();
+    Object[] constructorArgs = new Object[recordComponents.length];
+    for (int i = 0; i < recordComponents.length; i++) {
+      var component = recordComponents[i];
+      String componentName = component.getName();
+      Class<?> componentType = component.getType();
+
+      Object value = parametersMap.get(componentName);
+      Object convertedValue = ConversionUtils.convertStringOrArrToType(value, componentType);
+
+      if (componentType.isPrimitive() && convertedValue == null) {
+        // Skip setting primitive fields to null to avoid exceptions
+        System.out.println(
+            "[Fruits] : Skipping setting primitive record component to null: " + componentName + " on type: "
+                + targetType.getName());
+        continue;
+      }
+
+      constructorArgs[i] = convertedValue;
+    }
+
+    try {
+      Constructor<T> constructor = targetType.getDeclaredConstructor(
+          java.util.Arrays.stream(recordComponents)
+              .map(rc -> rc.getType())
+              .toArray(Class[]::new));
+      constructor.setAccessible(true);
+      return constructor.newInstance(constructorArgs);
+    } catch (NoSuchMethodException e) {
+      throw new ConversionObjectException("No matching constructor found for record type: " + targetType.getName(), e);
+    } catch (Exception e) {
+      throw new ConversionObjectException("Failed to instantiate record of type: " + targetType.getName(), e);
+    }
   }
 
   private static <T> T instanciate(Class<T> targetType, Object outerInstance) {
