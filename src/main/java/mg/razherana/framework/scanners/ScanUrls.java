ef6eaf2fc94f15aa1c6609e5e11f8a10ff3eacde
razherana <razherana@gmail.com>
2026-02-17 10:55:07 +0300

    fix: Made the urls support override

--- src/main/java/mg/razherana/framework/scanners/ScanUrls.java
+++ src/main/java/mg/razherana/framework/scanners/ScanUrls.java
@@ -2,23 +2,45 @@ package mg.razherana.framework.scanners;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import mg.razherana.framework.web.annotations.Url;
 import mg.razherana.framework.web.utils.ReflectUtils;
 
 class ScanUrls {
   static List<Method> findUrlMethodsInController(Class<?> clazz) {
-    // Get all methods of the class
-    List<Method> methods = ReflectUtils.getAllMethods(clazz, Object.class);
-    List<Method> urlMethods = new ArrayList<>();
+    List<Method> allMethods = ReflectUtils.getAllMethods(clazz, Object.class);
+    Map<Integer, Method> methodMap = new HashMap<>();
 
-    // Iterate through methods and check for @Url annotation
-    for (Method method : methods)
-      if (method.isAnnotationPresent(Url.class))
-        urlMethods.add(method);
+    // Process in reverse order (subclasses first)
+    List<Method> reversed = new ArrayList<>(allMethods);
+    Collections.reverse(reversed);
 
-    return urlMethods;
+    for (Method method : reversed) {
+      if (!method.isAnnotationPresent(Url.class)) {
+        continue;
+      }
+
+      // Create a hash based on method signature that accounts for overrides
+      int methodHash = getMethodOverrideHash(method);
+
+      // Only keep the first (most specific) occurrence
+      methodMap.putIfAbsent(methodHash, method);
+    }
+
+    return new ArrayList<>(methodMap.values());
+  }
+
+  private static int getMethodOverrideHash(Method method) {
+    // This creates a hash that will be the same for overriding methods
+    int result = method.getName().hashCode();
+    for (Class<?> paramType : method.getParameterTypes()) {
+      result = 31 * result + paramType.getName().hashCode();
+    }
+    return result;
   }
 
 }
